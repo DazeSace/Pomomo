02891ab078ace0645a6e987397c1617e4dc8f5ed
dazesace <dazemcblaze@gmail.com>
2021-06-15 18:04:38 +0200

    sound adjustments -7

--- bot/main.py
+++ bot/main.py
@@ -17,9 +17,8 @@ bot = commands.Bot(command_prefix=config.CMD_PREFIX, help_command=None, intents=
 
 if __name__ == '__main__':
     for filename in os.listdir(config.COGS_PATH):
-        if filename.endswith('.py'):
-            bot.load_extension(f'cogs.{filename[:-3]}')
-            print(f'Loaded cogs.{filename[:-3]}')
+        bot.load_extension(f'cogs.{filename[:-3]}')
+        print(f'Loaded cogs.{filename[:-3]}')
 
 
 @bot.event
